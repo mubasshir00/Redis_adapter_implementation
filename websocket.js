7e0819da558d7c3e70b82fea9e9ed2154ecb74b2
Mubasshir Ahmed <30567773+mubasshir00@users.noreply.github.com>
2022-09-07 08:49:53 +0600

    Update websocket.js

--- websocket.js
+++ websocket.js
@@ -3,11 +3,26 @@ const socket = require('socket.io'); //requires socket.io module
 const fs = require('fs');
 const cors = require('cors');
 const app = express();
-
+const redis = require('redis');
 const https = require('https');
+const config = require('config');
+const moment = require('moment');
+// const { redisClient } = require('./library/redis_connect');
+const redisClient = redis.createClient({
+  port: "6379",
+  host: "127.0.0.1",
+});
+const { createAdapter } = require('@socket.io/redis-adapter');
+const { Emitter } = require('@socket.io/redis-emitter');
 
 const httpPort = 5556;
 const httpsPort = 444;
+
+const pubClient = redisClient.duplicate();
+const subClient = redisClient.duplicate();
+
+// console.log({pubClient});
+// console.log({ subClient });
 
 // let credentials = {
 //     key: fs.readFileSync('ssl/new/new_private.key'),
@@ -15,11 +30,17 @@ const httpsPort = 444;
 //     ca: fs.readFileSync('ssl/new/new__elivehive_com.ca-bundle')
 // };
 
+const credentials = {
+    key: fs.readFileSync('ssl/private.key'),
+    cert: fs.readFileSync('ssl/bundle.crt'),
+    requestCert: true,
+    rejectUnauthorized: false,
+};
 
 // app.use(cors());
 
 const port = httpsPort;
-var server = https.createServer(app);
+var server = https.createServer(credentials, app);
 var con = server.listen(port, () => { console.log(`socket server listening on ${port}`) });
 
 /* var PORT = process.env.PORT || 5555;
@@ -38,9 +59,72 @@ const io = socket(con, {
     }
 });
 
+io.adapter(createAdapter(pubClient,subClient));
+
+const emitter = new Emitter(redisClient);
+
+// setInterval(() => {
+//   emitter.emit("time", new Date());
+// }, 5000);
+
+ function leaderboard_get(){
+
+    // return "HELLO"
+
+    return pubClient.zrevrange(
+      "host_leaderboard_lifetime",
+      "0",
+      "-1",
+      "WITHSCORES",
+      (err, data) => {
+        data.map(x => {
+          console.log(x);
+        });
+      }
+    );
+    // socket.adapter.emit("hello","world");
+    // pubClient.zrevrange(
+    //   "host_leaderboard_lifetime",
+    //   "0",
+    //   "-1",
+    //   "WITHSCORES",
+    //   (err, data) => {
+    //     if (err) {
+    //       return err;
+    //     }
+    //   }
+    // );
+}
+
+
+// function leaderboard_set(socket) {
+//   // console.log({ socket });
+
+//   subClient.zadd("test", "223", "1");
+
+//   pubClient.zrevrange("test", "0", "-1", "WITHSCORES", (err, data) => {
+//     data.map(x => {
+//       console.log(x);
+//       socket.emit("leaderboard", {
+//         all: x,
+//       });
+//     });
+//   });
+// }
+
+console.log(pubClient);
+
+
+
+
 //Socket.io Connection------------------
 io.on('connection', (socket) => {
-    console.log("New socket connection: " + socket.id);
+   
+    // leaderboard_set(socket);
+    // leaderboard_get();
+
+   socket.emit("leaderboard",leaderboard_get());
+
     socket.on('onMessage', (data) => {
         console.log({ data });
         socket.broadcast.emit('onMessageReceived', data);
@@ -49,4 +133,6 @@ io.on('connection', (socket) => {
     socket.on('disconnect', function () {
         console.log('disconnected: ' + socket.id);
     });
+
+    
 })
